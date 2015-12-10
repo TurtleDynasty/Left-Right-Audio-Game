dfb67566a6349c582d94ff40db7856f8c92571b8
DAVID <dm667@nau.edu>
2015-12-09 21:14:57 -0700

    Debugged skeleton code

--- app/src/main/java/com/example/david/leftright/MainActivity.java
+++ app/src/main/java/com/example/david/leftright/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.david.leftright;
 
+import android.content.Context;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     boolean isLeft = false;
+    int score = 0;
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,18 +41,22 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void getRandomBoolean(){
-        if(Math.random()>0.5)
-            Log.d("Debug", "true");
-        else
-            Log.d("Debug", "false");
+    public boolean getRandomBoolean(View v){
+        if(Math.random()>0.5) {
+            //Log.d("Debug", "true");
+            return true;
+        } else {
+            //Log.d("Debug", "false");
+            return false;
+        }
     }
 
-    public void clickLeftButton(){
-
+    public void clickLeftButton(View v){
+        Log.d("MainActivity", "left");
     }
 
-    public void clickRightButton(){
+    public void clickRightButton(View v){
+        Log.d("MainActivity", "right");
 
     }
 
